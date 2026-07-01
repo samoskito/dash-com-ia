0b85762928d2828e30c3ef5cdbc416c0e316b0f9
samoskito <samuel.choairy.a@gmail.com>
2026-07-01 20:47:56 -0300

    feat: add Prisma data foundation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "./common/prisma/prisma.service";
 import { HealthController } from "./health/health.controller";
 import { MockController } from "./mock/mock.controller";
 import { MockService } from "./mock/mock.service";
 
 @Module({
   controllers: [HealthController, MockController],
-  providers: [MockService]
+  providers: [MockService, PrismaService]
 })
 export class AppModule {}
